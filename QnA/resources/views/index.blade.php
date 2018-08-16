3fe56d16f316ebf294d85a8e0a2db87529d5f450
Yang8547 <anyang47@hotmail.com>
2018-08-16 11:15:22 -0400

    ui basic layout

---
+++ QnA/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>QnA</title>
+	<link rel="stylesheet" type="text/css" href="/node_modules/normalize-css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+	<script src="/node_modules/jquery/dist/jquery.js"></script>
+	<script src="/node_modules/angular/angular.js"></script>
+	<script src="/node_modules/angular-ui-router/release/angular-ui-router.js"></script>
+	<script src="js/app.js"></script>
+</head>
+<body>
+	Hello
+</body>
+</html>
